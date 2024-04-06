70104c1e7009cd75c8f83f8ee488a8fdab0b137b
Mamatha75@ <mamatha20@navgurukul.org>
2024-04-06 19:03:15 +0530

    reactjs commit

--- stock-screener/src/App.js
+++ stock-screener/src/App.js
@@ -1,17 +1,6 @@
-// import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from 'react';
 import HeaderMenu from './components/HeaderMenu';
-// import MenuBar from './components/MenuBar';
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-// import Filter from './components/Filter';
-// import Sectors from './components/Secors';
-
-// import SearchBar from './components/SearchBar';
-
-
-
 
 function App() {
   const [data, setData] = useState([]);
@@ -22,7 +11,7 @@ function App() {
 
   const fetchData = async () => {
     try {
-      const response = await fetch('https://restcountries.com/v3.1/all'); 
+      const response = await fetch('https://restcountries.com/v3.1/all');
       const jsonData = await response.json();
       setData(jsonData);
     } catch (error) {
@@ -32,39 +21,21 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      
-
         <h1>Stock Analysis App</h1>
-
       </header>
       <div>
-      <HeaderMenu />
-        {/* <p className="heading">
-          All Stocks
-        </p> */}
-      </div>
+        <HeaderMenu />
 
-      <div className="App">
-        {/* <MenuBar /> */}
-
-
-        {/* <BrowserRouter>
-          <Routes>
-          <Route path="/sectors" component={Sectors} />
-          </Routes>
-          </BrowserRouter> */}
       </div>
-      <div className="side-menu">
-        
-      </div>
+    
       <div>
-      <h1>List from API</h1>
-      <ul>
-        {data.map(item => (
-          <li key={item.id}>{item.name}</li> // Assuming each item has a unique 'id' and 'name' property
-        ))}
-      </ul>
-    </div>
+        <h1>List from API</h1>
+        <ul>
+          {data.map(item => (
+            <li key={item.id}>{item.name}</li> 
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
